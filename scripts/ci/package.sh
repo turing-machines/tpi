2ae3589b6475f61385a1fb4829375649da801e73
Sven Rademakers <sven.rademakers@gmail.com>
2023-07-24 11:16:24 +0100

    Github workflow: Added cross build matrix
    * added 3 build targets: x86_darwin, _msvc, _linux.
    * Added a job that lints the code.
    This commit does not package the binaries yet.

---
+++ scripts/ci/package.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -x
+
+packager=$1 
+
+package_win() {
+    # no packager yet
+    arch=echo $1|cut -d "-" -f 1
+    mkdir -p target/win/$arch
+    mv target/$1/release/tpi.exe target/win/$arch
+}
+
+package_macos() {
+    # no packager yet
+    arch=echo $1|cut -d "-" -f 1
+    mkdir -p target/apple/$arch
+    mv target/$1/release/tpi target/apple/$arch
+}
+
+package_deb() {
+    echo "target: $1"
+    cargo install cargo-deb
+    cargo-deb --target $1 --no-build\
+        --no-strip
+    }
+
+    package_arch() { 
+        cargo install cargo-aur
+        cargo aur
+        mkdir -p target/$1/arch
+        mv *.tar.gz PKGBUILD target/$1/arch
+    }
+
+
+    for target in $(ls target| grep linux)
+    do
+        target_name=$(echo $target | cut -d "/" -f 2)
+        case $packager in
+            "debian")
+                package_deb $target_name
+                ;;
+            "arch")
+                package_arch $target_name
+                ;;
+        esac
+    done
+
+    for target in $(ls target| grep windows)
+    do
+        target_name=$(echo $target | cut -d "/" -f 2)
+        case $packager in
+            "windows")
+                package_win $target_name
+                ;;
+        esac
+    done
+
+    for target in $(ls target| grep apple)
+    do
+        target_name=$(echo $target | cut -d "/" -f 2)
+        case $packager in
+            "macos")
+                package_macos $target_name
+                ;;
+        esac
+    done
+
