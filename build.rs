77ea6a2e76f43d3fd0dabced2b5ca37eb330595f
Sven Rademakers <sven.rademakers@gmail.com>
2023-08-02 10:38:58 +0100

    PKGBUILD: download tag'ed tarballs

--- build.rs
+++ build.rs
@@ -3,6 +3,7 @@ use std::io;
 use std::io::Write;
 
 fn main() -> io::Result<()> {
+    let pkg_name = format!("{}-{}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
     let mut pkgbuild = File::create(format!("{}/PKGBUILD", std::env::var("OUT_DIR").unwrap()))?;
     writeln!(pkgbuild, "# Maintainer: {}\n", env!("CARGO_PKG_AUTHORS"))?;
     writeln!(pkgbuild, "pkgname={}-git", env!("CARGO_PKG_NAME"))?;
@@ -15,43 +16,43 @@ fn main() -> io::Result<()> {
     writeln!(pkgbuild, "arch=('any')")?;
     writeln!(
         pkgbuild,
-        r"source=('{}-{}.tar.gz::{}/archive/refs/heads/master.tar.gz')",
-        env!("CARGO_PKG_NAME"),
-        env!("CARGO_PKG_VERSION"),
-        env!("CARGO_PKG_REPOSITORY")
-    )?;
-    writeln!(
-        pkgbuild,
-        r"sha256sums=('899f038bcf3d2baa99d6e21116aed3636cf2f0c2907252dcbbc0b522a212d97a')",
+        r"source=('{}/archive/refs/tags/{}.tar.gz')",
+        env!("CARGO_PKG_REPOSITORY"),
+        env!("CARGO_PKG_VERSION")
     )?;
     writeln!(
         pkgbuild,
         r#"prepare() {{
+    cd $srcdir/{}
     export RUSTUP_TOOLCHAIN=stable
     cargo fetch --locked --target "$CARCH-unknown-linux-gnu"
 }}
-"#
+"#,
+        pkg_name
     )?;
     writeln!(
         pkgbuild,
         r#"build() {{
     export RUSTUP_TOOLCHAIN=stable
     export CARGO_TARGET_DIR=target
-    cargo build --frozen --release --all-features
-}}"#
+    cd $srcdir/{} && cargo build --frozen --release --all-features
+}}"#,
+        pkg_name
     )?;
     writeln!(
         pkgbuild,
         r#"check() {{
     export RUSTUP_TOOLCHAIN=stable
-    cargo test --frozen --all-features
-}}"#
+    cd $srcdir/{} && cargo test --frozen --all-features
+}}"#,
+        pkg_name
     )?;
     writeln!(
         pkgbuild,
         r#"package() {{
-    install -Dm0755 -t "$pkgdir/usr/bin/" "target/release/{}"
+    install -Dm0755 -t "$pkgdir/usr/bin/" "$srcdir/{}/target/release/{}"
 }}"#,
+        pkg_name,
         env!("CARGO_PKG_NAME")
     )?;
     Ok(())
