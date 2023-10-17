040d2a8186379cacf24656f68222825c6ff129d4
Ruslan Akbashev <ra@turingpi.com>
2023-10-17 08:51:03 +0400

    add querying, using and caching auth credentials

---
+++ src/prompt.rs
@@ -0,0 +1,146 @@
+use std::io::{stdout, Write};
+
+use anyhow::{bail, Result};
+use crossterm::cursor::MoveToColumn;
+use crossterm::event::{self, Event, KeyCode, KeyModifiers};
+use crossterm::style::Print;
+use crossterm::terminal::{disable_raw_mode, enable_raw_mode, Clear, ClearType};
+use crossterm::{execute, queue};
+
+struct Prompt {
+    msg: &'static str,
+    password: bool,
+    input: String,
+    cursor_idx: usize,
+}
+
+impl Prompt {
+    fn new(msg: &'static str, password: bool) -> Self {
+        Self {
+            msg,
+            password,
+            input: String::new(),
+            cursor_idx: 0,
+        }
+    }
+
+    fn read(&mut self) -> Result<String> {
+        enable_raw_mode()?;
+
+        let res = self.read_loop();
+
+        disable_raw_mode()?;
+
+        match res {
+            Ok(()) => Ok(self.input.clone()),
+            Err(e) => bail!("failed to get terminal event: {e}"),
+        }
+    }
+
+    fn read_loop(&mut self) -> Result<()> {
+        execute!(stdout(), event::EnableBracketedPaste)?;
+
+        loop {
+            self.print()?;
+
+            let cont = match event::read()? {
+                Event::Key(key) => self.handle_key(key)?,
+                Event::Paste(text) => self.handle_paste(&text),
+                _ => true,
+            };
+
+            if !cont {
+                break;
+            }
+        }
+
+        execute!(stdout(), event::DisableBracketedPaste)?;
+
+        Ok(())
+    }
+
+    fn print(&self) -> Result<()> {
+        queue!(
+            stdout(),
+            MoveToColumn(0),
+            Clear(ClearType::CurrentLine),
+            Print(format!("{}: ", self.msg)),
+        )?;
+
+        if !self.password {
+            let column = self.msg.len() + self.cursor_idx + 2;
+            let column = u16::try_from(column).unwrap_or(0);
+
+            queue!(stdout(), Print(&self.input), MoveToColumn(column))?;
+        }
+
+        stdout().flush()?;
+
+        Ok(())
+    }
+
+    fn handle_key(&mut self, key: event::KeyEvent) -> Result<bool> {
+        let interrupt = key.modifiers == KeyModifiers::CONTROL && key.code == KeyCode::Char('c');
+
+        if interrupt || key.code == KeyCode::Enter {
+            execute!(stdout(), Print("\n\r"))?;
+            return Ok(false);
+        }
+
+        match key.code {
+            KeyCode::Char(c) => {
+                self.input.insert(self.cursor_idx, c);
+                self.cursor_idx += 1;
+            }
+            KeyCode::Delete => {
+                if !self.input.is_empty() {
+                    self.delete();
+                }
+            }
+            KeyCode::Backspace => {
+                if !self.input.is_empty() {
+                    self.left();
+                    self.delete();
+                }
+            }
+            KeyCode::Left => self.left(),
+            KeyCode::Right => {
+                if self.cursor_idx < self.input.len() - 1 {
+                    self.cursor_idx += 1;
+                }
+            }
+            _ => {}
+        }
+
+        Ok(true)
+    }
+
+    fn left(&mut self) {
+        if self.cursor_idx > 0 {
+            self.cursor_idx -= 1;
+        }
+    }
+
+    fn delete(&mut self) {
+        if self.cursor_idx < self.input.len() {
+            self.input.remove(self.cursor_idx);
+        }
+    }
+
+    fn handle_paste(&mut self, text: &str) -> bool {
+        if text.chars().all(|c| c != '\n') {
+            self.input.push_str(text);
+            self.cursor_idx += text.len();
+        }
+
+        true
+    }
+}
+
+pub fn simple(msg: &'static str) -> Result<String> {
+    Prompt::new(msg, false).read()
+}
+
+pub fn password(msg: &'static str) -> Result<String> {
+    Prompt::new(msg, true).read()
+}
